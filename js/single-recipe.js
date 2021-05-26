c81b2c6e08417f2b110771018b339fa9c05926f0
Jessica <jessicakchell@gmail.com>
2021-05-26 12:39:51 +0200

    created image modal - html, css, js

--- js/single-recipe.js
+++ js/single-recipe.js
@@ -21,8 +21,28 @@ async function getRecipe(recipeId) {
 		document.querySelector('.single-recipe-container').innerHTML += `
         		<h1 class="single-recipe-heading">${recipeInfo.title.rendered}</h1>
                 <img class="recipe__image" id="imageModal" alt="${recipeInfo.title.rendered}" src="${recipeInfo['featured_media_src_url']}">
+					<div id="modalContainer" class="modal">
+						<span class="close-modal"><i class="fas fa-times-circle"></i></span>
+						<img class="modal-content" id="image1">
+						<div id="imageCaption" class="image-caption"></div>
+					</div>
                 ${recipeInfo.content.rendered}
     		`;
+
+		var modal = document.getElementById('modalContainer');
+		var image = document.getElementById('imageModal');
+		var modalImg = document.getElementById('image1');
+		var captionText = document.getElementById('imageCaption');
+		image.onclick = function () {
+			modal.style.display = 'block';
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		};
+
+		var span = document.getElementsByClassName('close-modal')[0];
+		span.onclick = function () {
+			modal.style.display = 'none';
+		};
 	} catch (error) {
 		document.querySelector('#alert').innerHTML = showAlertTouser(
 			'An Error occured',
